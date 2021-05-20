2d51922f980355ee87f45ece58cfa34cb8ed9181
Jaric Woodhead <jaric_woodhead@msn.com>
2021-05-20 12:33:43 +0100

    Styled the image view

--- src/Components/ClimbView.js
+++ src/Components/ClimbView.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 const ClimbView = (props) => {
     return (
-        <div className="fixed flex justify-center w-full h-screen bg-yellow-700">
+        <div className="fixed flex justify-center w-full h-screen bg-gray-700">
       <img src={props.newImage} className="max-h-screen" />
+      <div className="absolute bottom-6 bg-gray-900 rounded-lg flex flex-col items-center p-2 border-2 border-yellow-600 w-64">
+      <h2 className=" text-2xl font-Bubblegum text-yellow-600 p-1">{props.name}</h2>
+      <h2 className=" text-lg font-Merriweather text-yellow-600 p-1">{props.grade}</h2>
       <button
-        className="absolute bottom-10 bg-gray-700 p-4 rounded-lg text-white"
+        className=" font-Merriweather border-yellow-600 border-2 rounded-lg p-4 px-6 w-32 hover:bg-yellow-600 hover:text-white bg-gray-900 text-yellow-600"
+      
         onClick={() => props.setView([])}
       >
         Go Back
       </button>
+      </div>
     </div>
     )
 }
--- src/Components/Homescreen.js
+++ src/Components/Homescreen.js
@@ -18,8 +18,8 @@ import PocketDial from "../Images/PocketDial.jpg";
 const Test = () => {
   const [view, setView] = useState([]);
 
-  const findImage = (img) => {
-    setView(<ClimbView setView={setView} newImage={img} />);
+  const findImage = (img, name, grade) => {
+    setView(<ClimbView setView={setView} newImage={img} name={name} grade={grade} />);
   };
 
   return (
@@ -32,63 +32,63 @@ const Test = () => {
         <div className="w-10/12 md:w-8/12 bg-gray-600 rounded-lg p-4 flex flex-col items-center mb-6">
           <NewClimb
             name={"Yellow Submarine"}
-            grade={"6a+"}
+            grade={"6A+"}
             stars={"***"}
-            function={() => findImage(YellowSubmarine)}
+            function={() => findImage(YellowSubmarine, "Yellow Submarine", "6A+")}
           />
           <NewClimb
             name={"Hamilton"}
-            grade={"6b+"}
+            grade={"6B+"}
             stars={"***"}
-            function={() => findImage(Hamilton)}
+            function={() => findImage(Hamilton, "Hamilton", "6B+")}
           />
           <NewClimb
             name={"Red October"}
-            grade={"6b"}
+            grade={"6B"}
             stars={"**"}
-            function={() => findImage(RedOctober)}
+            function={() => findImage(RedOctober, "Red October", "6B")}
           />
           <NewClimb
             name={"Shooda Wooda Cooda"}
-            grade={"6c"}
+            grade={"6C"}
             stars={"**"}
-            function={() => findImage(ShoodaWoodaCooda)}
+            function={() => findImage(ShoodaWoodaCooda, "Shooda Wooda Cooda", "6C")}
           />
           <NewClimb
             name={"The Long Arm of the Law"}
-            grade={"6c+"}
+            grade={"6C+"}
             stars={"***"}
-            function={() => findImage(TheLongArmoftheLaw)}
+            function={() => findImage(TheLongArmoftheLaw, "The Long Arm of the Law", "6C+")}
           />
           <NewClimb
             name={"The Big Squeeze"}
-            grade={"6c"}
+            grade={"6C"}
             stars={"**"}
-            function={() => findImage(TheBigSqueeze)}
+            function={() => findImage(TheBigSqueeze, "The Big Squeeze", "6C")}
           />
           <NewClimb
             name={"Left Behind"}
-            grade={"7a"}
+            grade={"7A"}
             stars={"**"}
-            function={() => findImage(LeftBehind)}
+            function={() => findImage(LeftBehind, "Left Behind", "7A")}
           />
           <NewClimb
             name={"Grim Reaper"}
-            grade={"7b"}
+            grade={"7B"}
             stars={"**"}
-            function={() => findImage(GrimReaper)}
+            function={() => findImage(GrimReaper, "Grim Reaper", "7B")}
           />
           <NewClimb
             name={"Pocket Dial"}
-            grade={"7a+"}
+            grade={"7A+"}
             stars={"**"}
-            function={() => findImage(PocketDial)}
+            function={() => findImage(PocketDial, "Pocket Dial", "7A+")}
           />
           <NewClimb
             name={"Lock 'n' Load"}
-            grade={"7b"}
+            grade={"7B"}
             stars={"***"}
-            function={() => findImage(LockNLoad)}
+            function={() => findImage(LockNLoad, "Lock 'n' Load", "7B")}
           />{" "}
         </div>
       </section>
